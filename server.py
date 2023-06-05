c343d305f7ced6142e73c51960b1ff61c91df3c1
ken <59551949+ktroima@users.noreply.github.com>
2023-06-05 19:17:59 +0900

    customer関連を削除

--- server.py
+++ server.py
@@ -7,14 +7,6 @@ import flask_app.views.staff.staff_customer
 import flask_app.views.staff.staff_login
 import flask_app.views.staff.staff_reservation
 import flask_app.views.staff.staff_ticket
-import flask_app.views.customer.customer_account
-import flask_app.views.customer.common.customer_common
-import flask_app.views.customer.customer_login
-import flask_app.views.customer.customer_reservation
-import flask_app.views.customer.customer_top
-import flask_app.views.customer.delete_account
-import flask_app.views.customer.event_detail
-import flask_app.views.customer.mypage_menu
 
 app.secret_key = 'hoge'
 
